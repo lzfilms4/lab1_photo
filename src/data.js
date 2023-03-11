8003e8736096cf601f6e0402685e06b9d49ebea5
Artem Zakharov <35864884+Zaxarov@users.noreply.github.com>
2023-03-11 21:29:52 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -44,6 +44,84 @@ const data = {
         "photo": 
           "https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
           
+      },
+        {
+        "category": 5,
+        "name": "близняшки Atomic Heart",
+        "photo":
+          "https://playerok.fra1.digitaloceanspaces.com/images/1edbf59c-0294-6c80-e737-60e0973f29b2.jpg"
+      },
+      {
+        "category": 5,
+        "name": "Atomic Heart",
+        "photo":
+            "https://images.stopgame.ru/news/2022/11/23/A4dnHsAF.jpg"
+      },
+      {
+        "category": 5,
+        "name": "World of Warcraft",
+        "photo":
+            "https://cs8.pikabu.ru/post_img/big/2016/11/28/11/1480362328121070648.jpg"
+      },
+      {
+        "category": 5,
+        "name": "dota 2",
+        "photo":
+            "https://www.meme-arsenal.com/memes/919b70b522ef661070bd6c8e41b8d3dd.jpg"
+      },
+      {
+        "category": 5,
+        "name": "CS:GO",
+        "photo":
+            "https://pixelbox.ru/wp-content/uploads/2021/07/ava-youtube-cs-go-1-scaled.jpeg"
+      },
+      {
+        "category": 5,
+        "name": "Warhammer",
+        "photo":
+            "https://img4.goodfon.ru/wallpaper/nbig/9/67/blood-angels-space-marine-wh40k-warhammer-40k-voiny-soldaty.jpg"
+      },
+      {
+        "category": 6,
+        "name": "Блич",
+        "photo":
+          "https://kartinkin.net/uploads/posts/2021-07/1626447808_3-kartinkin-com-p-anime-blich-ichigo-anime-krasivo-4.jpg"
+
+      },
+      {
+        "category": 6,
+        "name": "Наруто",
+        "photo":
+            "https://kartinki-srisovki.ru/wp-content/uploads/glavnaya-15.jpg"
+
+      },
+      {
+        "category": 6,
+        "name": "One Piece",
+        "photo":
+            "https://winpreso.com/wp-content/uploads/2021/01/wallpaper-anime-one-piece-windows-10_4K.jpg"
+
+      },
+      {
+        "category": 6,
+        "name": "Волчий дождь",
+        "photo":
+            "https://images.squarespace-cdn.com/content/v1/54fc8146e4b02a22841f4df7/1505927044554-TQAF0Z2PTO5SVVGNM2MR/wolf.jpg"
+
+      },
+      {
+        "category": 6,
+        "name": "Манки Д. Луффи",
+        "photo":
+            "https://virtus-img.cdnvideo.ru/images/og-jpg/plain/e9/e98503a2-5c44-4013-b1e9-9db3792108a2.jpg"
+
+      },
+      {
+        "category": 6,
+        "name": "Старшая школа DxD",
+        "photo":
+            "https://avatars.mds.yandex.net/i?id=208e8c94686ed26aaaf9b32a56d1f5719cd055f2-8497298-images-thumbs&n=13"
+
       }
     ]
   }
